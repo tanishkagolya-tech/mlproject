24bd68d726828ad67fbfa6790089a959b68813b4
tanishkagolya-tech <tanishkagolya224@gmail.com>
2026-02-25 17:41:03 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Tanishka',
+author_email='tanishkagolya224@gmail.com',
+packages=find_packages(),
+install_Requires=get_requirements('requirement.txt')
+
+)
