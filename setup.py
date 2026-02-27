8803a7520bbdd5e501159b633e37a3df70376065
tanishkagolya-tech <tanishkagolya224@gmail.com>
2026-02-27 17:06:20 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function will return the list of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 
 
 setup(
-name='mlproject',
-version='0.0.1',
-author='Tanishka',
-author_email='tanishkagolya224@gmail.com',
-packages=find_packages(),
-install_Requires=get_requirements('requirement.txt')
-
+    name='mlproject',
+    version='0.0.1',
+    author='Tanishka',
+    author_email='tanishkagolya224@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
