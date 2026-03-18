020e54f331b74f2719543d8f66f9b01a71b9cca8
tanishkagolya-tech <tanishkagolya224@gmail.com>
2026-03-19 02:11:55 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -19,5 +19,11 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
-    
+
+
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        custom_error = CustomException(e, sys)
+        logging.error(custom_error)  
